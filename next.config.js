93d1fa56cda621be4f0b890f69f3e1d228136757
inovaprog <GITEMAIL>
2021-10-08 00:51:42 -0300

    url api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-      URL: 'https://lbm7oy8h52.execute-api.us-east-1.amazonaws.com/production'
+      URL: 'https://rx5p46eai7.execute-api.us-east-1.amazonaws.com/development'
       //URL: 'http://localhost:5000'
     },
   }
