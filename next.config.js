091a4b581096504e4c25d45792c924f4046aea38
Wandinho Oliveira <inovaprog@gmail.com>
2021-11-16 00:08:57 -0300

    new url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-      URL: 'https://rx5p46eai7.execute-api.us-east-1.amazonaws.com/development'
+      URL: 'https://nd1z5y1mg2.execute-api.us-east-1.amazonaws.com/production'
       //URL: 'http://localhost:5000'
     },
   }
